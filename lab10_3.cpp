ff253e3cfc357a466f4e284d0047e4f4c0183612
lab10aommie <ananya.aomsinxz@gmail.com>
2024-12-17 14:49:36 +0700

    edit

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,45 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int count = 0;  
+    float sum = 0.0; 
+    float sum_of_square = 0.0; 
+    string textline;
+    ifstream source("score.txt"); 
+
+    if (!source) {
+        cout << "Unable to open file score.txt" << endl;
+        return 1; 
+    }
+
+    while (getline(source, textline)) { 
+        float score = stof(textline); 
+        sum += score; 
+        sum_of_square += score * score; 
+        count++; 
+    }
+
+    if (count == 0) {
+        cout << "No data found in the file." << endl;
+        return 1; 
+    }
+
+    float mean = sum / count;
+
+    float variance = (sum_of_square / count) - (mean * mean);
+    float standard_deviation = sqrt(variance);
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);  
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << standard_deviation << "\n";
+
+    source.close();
+    return 0;
+}
